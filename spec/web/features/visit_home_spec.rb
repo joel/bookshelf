1141c5ca234ddbf034d29f3e46ec30654bd95ca0
Joel Azemar <joel.azemar@gmail.com>
2022-03-25 17:47:23 +0100

    Add Feature Test Visit Home

---
+++ spec/web/features/visit_home_spec.rb
@@ -0,0 +1,12 @@
+# frozen_string_literal: true
+
+# spec/web/features/visit_home_spec.rb
+require "features_helper"
+
+RSpec.describe "Visit home" do
+  it "is successful" do
+    visit "/"
+
+    expect(page).to have_content("Bookshelf")
+  end
+end
