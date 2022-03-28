30101673be908727d81b5a3a76a250be77875fcb
Joel Azemar <joel.azemar@gmail.com>
2022-03-28 13:55:12 +0200

    Adding Book Entity

---
+++ spec/bookshelf/entities/book_spec.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+# spec/bookshelf/entities/book_spec.rb
+
+RSpec.describe Book, type: :entity do
+  it "can be initialized with attributes" do
+    book = Book.new(title: "Refactoring", author: "Martin Fowler")
+    expect(book.title).to eq("Refactoring")
+    expect(book.author).to eq("Martin Fowler")
+  end
+end
---
+++ spec/bookshelf/repositories/book_repository_spec.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+RSpec.describe BookRepository, type: :repository do
+  # place your tests here
+end
